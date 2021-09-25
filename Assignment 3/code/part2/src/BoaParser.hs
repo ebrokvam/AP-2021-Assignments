d9901f2869b3426362a8b7bd2680a413617d4004
albeate <blaadino@protonmail.com>
2021-09-25 17:55:11 +0200

    it can parse now

--- Assignment 3/code/part2/src/BoaParser.hs
+++ Assignment 3/code/part2/src/BoaParser.hs
@@ -27,7 +27,7 @@ parseString :: String -> Either ParseError Program
 parseString = parse stmtS "parse error"
 
 stmtS :: Parser Program
-stmtS = do s <- stmt; return [s]
+stmtS = do s <- stmt; eof; return [s]
        <|> do s' <- stmt; symbol ";"; s'' <- stmtS; return (s':s'')
 
 stmt :: Parser Stmt
@@ -36,66 +36,53 @@ stmt = do v <- ident; symbol "="; SDef v <$> exprS
 
 
 exprS :: Parser Exp
-exprS = do e <- expr; eof; return e
-       <|> do e' <- expr; symbol ","; e'' <- exprS; return $ List [e',e'']
+-- exprS = do e <- expr; eof; return e
+exprS = expr'
+       <|> do e' <- expr'; symbol ","; e'' <- exprS; return $ List [e',e'']
+
+expr' :: Parser Exp
+expr' = do e <- expr; negOp e
 
 expr :: Parser Exp
 expr = do e <- term'; relOp e
--- expr = term' `chainl1` relOp
 
 term' :: Parser Exp
 term' = do t' <- term; addOp t'
 
 term :: Parser Exp
-term = do t <- factor; multOp t
+term = do t <- factor'; multOp t
+
+factor' :: Parser Exp
+factor' = factor
+       <|> do symbol "-"; Not <$> factor
 
 factor :: Parser Exp
 factor = Const . IntVal <$> numConst
        <|> Var <$> ident
        <|> do symbol "("; f <- exprS; symbol ")"; return f
-       -- <|> do symbol "'"; f <- stringConst; symbol "'"; return $ Const (StringVal f)
-
-{-
-overvej at gøre noget ligende 
--- https://stackoverflow.com/questions/16367150/how-to-make-the-parsec-chainl1-function-follow-operator-precedence-rules
-
-dvs. split alle Op op i hver deres foo og på den måde gøre brug af `chainl1`
-
--}
--- -- relOp :: Parser (Exp -> Exp -> Exp)
--- relOp   = do symbol "=="; Oper Eq 
---       <|> do symbol ">";  Oper Greater
---       <|> do symbol "<";  Oper Less 
---       <|> do symbol "in"; Oper In 
---       -- <|> do symbol "not"; Not  
-
--- addOp :: Exp -> Parser Exp
--- addOp e = do symbol "+"; Oper Plus e  <$> exprS
---       <|> do symbol "-"; Oper Minus e <$> exprS 
-
--- multOp :: Exp -> Parser Exp
--- multOp e = do symbol "*";  Oper Times e <$> exprS 
---        <|> do symbol "//"; Oper Div e   <$> exprS
---        <|> do symbol "%";  Oper Mod e   <$> exprS
-
+       <|> do symbol "'"; f <- stringConst; symbol "'"; return $ Const (StringVal f)
 
 relOp :: Exp -> Parser Exp
 relOp e = do symbol "=="; Oper Eq e      <$> exprS 
       <|> do symbol ">";  Oper Greater e <$> exprS
       <|> do symbol "<";  Oper Less e    <$> exprS
       <|> do symbol "in"; Oper In e      <$> exprS
-      <|> do symbol "not"; Not           <$> exprS
+      <|> return e
 
 addOp :: Exp -> Parser Exp
 addOp e = do symbol "+"; Oper Plus e  <$> exprS
       <|> do symbol "-"; Oper Minus e <$> exprS 
+      <|> return e
 
 multOp :: Exp -> Parser Exp
 multOp e = do symbol "*";  Oper Times e <$> exprS 
        <|> do symbol "//"; Oper Div e   <$> exprS
        <|> do symbol "%";  Oper Mod e   <$> exprS
+       <|> return e
 
-
+negOp :: Exp -> Parser Exp
+negOp e = do symbol "not"; negOp $ Not e
+      <|> return e
 
 ident :: Parser String
 ident = lexeme $ 
@@ -112,11 +99,12 @@ stringConst :: Parser String
 stringConst = lexeme $ do s <- many1 letter; spaces; return $ read s
 
 
+-- do not use!!
 -- whitespace :: Parser ()
 -- whitespace = do _ <- space; _ <- newline; _ <- tab; return ()
 
 lexeme :: Parser a -> Parser a
-lexeme p = do a <- p; spaces; return a
+lexeme p = do a <- p; spaces; do _ <- optional comments; return a
 
 symbol :: String -> Parser ()
 symbol ps = lexeme $ do _ <- try $ string ps; spaces
@@ -124,3 +112,6 @@ symbol ps = lexeme $ do _ <- try $ string ps; spaces
 
 reserved :: [String]
 reserved = ["None", "True", "False", "for", "if", "in", "not"]
+
+comments :: Parser String
+comments = do _ <- string "#"; _ <- manyTill anyChar newline; return ""
