3aaf0bef375028e39d0fc59c34061be4b0b376a2
Espen <ebrokvam@gmail.com>
2021-09-29 16:12:41 +0200

    My version + tests

--- Assignment 3/code/part2/src/BoaParser.hs
+++ Assignment 3/code/part2/src/BoaParser.hs
@@ -3,115 +3,124 @@
 module BoaParser (ParseError, parseString) where
 
 import BoaAST
-import Text.ParserCombinators.Parsec  -- exports a suitable type ParseError
-import Text.ParserCombinators.Parsec.Prim()
 -- add any other other imports you need
 
-{-
-these submodules are allowed
-  - Prim        - tjek
-  - Char
-  - Error
-  - String
-  - Combinator 
-
-these submodules are NOT allowed (sad:face)
-  - Token
-  - Expr (has buildExpressionParser - so so nice functionality)
-  - Language
--}
-
--- type ParseError = String -- you may replace this
+import Text.ParserCombinators.Parsec
+import Data.Char
 
 parseString :: String -> Either ParseError Program
-parseString = parse stmtS "parse error"
+parseString = parse parseBoa "parse error"
 
-stmtS :: Parser Program
-stmtS = do s <- stmt; eof; return [s]
-       <|> do s' <- stmt; symbol ";"; s'' <- stmtS; return (s':s'')
+
+parseBoa :: Parser Program
+parseBoa = do spaces; p <- stmts; eof; return p
+
+stmts :: Parser [Stmt]
+stmts = try (do s <- stmt; symbol ";"; ss <- stmts; return (s:ss))
+    <|> do s <- stmt; return [s]
 
 stmt :: Parser Stmt
-stmt = do v <- ident; symbol "="; SDef v <$> exprS
-      <|> do SExp <$> exprS
-
-
-exprS :: Parser Exp
--- exprS = do e <- expr; eof; return e
-exprS = expr'
-       <|> do e' <- expr'; symbol ","; e'' <- exprS; return $ List [e',e'']
-
-expr' :: Parser Exp
-expr' = do e <- expr; negOp e
+stmt = try (do i <- ident; symbol "="; e <- expr; return $ SDef i e)
+    <|> do e <- expr; return (SExp e)
 
 expr :: Parser Exp
-expr = do e <- term'; relOp e
+expr = lexeme $ try (do e <- expr'; (not, o, e') <- oper; return $ if not then Not (Oper o e e') else Oper o e e' )
+    <|> do expr'
 
-term' :: Parser Exp
-term' = do t' <- term; addOp t'
+expr' :: Parser Exp
+expr' = lexeme $ do n <- numConst; return (Const (IntVal n))
+    <|> do symbol "\'"; s <- stringConst; return (Const (StringVal s))
+    <|> do symbol "None"; return (Const NoneVal)
+    <|> do symbol "True"; return (Const TrueVal)
+    <|> do symbol "False"; return (Const FalseVal)
+    <|> try (do string "not"; requiredSpaces; e <- expr; return (Not e))
+    <|> try (do i <- ident; symbol "("; es <- exprz; symbol ")"; return (Call i es))
+    <|> do i <- ident; return (Var i)
+    <|> do symbol "("; e <- expr; symbol ")"; return e
+    <|> try (do symbol "["; es <- exprz; symbol "]"; return (List es))
+    <|> do symbol "["; e <- expr; fc <- forClause; cs <- clausez; char ']'; return (Compr e (fc:cs))
 
-term :: Parser Exp
-term = do t <- factor'; multOp t
+oper :: Parser (Bool, Op, Exp)
+oper = try (do symbol "!="; e' <- expr; return $ (True, Eq, e'))
+    <|> try (do symbol "<="; e' <- expr; return $ (True, Greater, e'))
+    <|> try (do symbol ">="; e' <- expr; return $ (True, Less, e'))
+    <|> try (do symbol "not in"; e' <- expr; return $ (True, In, e'))
+    <|> do o <- operType; e' <- expr; return $ (False, o, e')
 
-factor' :: Parser Exp
-factor' = factor
-       <|> do symbol "-"; Not <$> factor
+operType :: Parser Op
+operType = lexeme $ do char '+'; return Plus
+    <|> do char '-'; return Minus
+    <|> do char '*'; return Times
+    <|> do string "//"; return Div
+    <|> do char '%'; return Mod
+    <|> do string "=="; return Eq;
+    <|> do char '<'; return Less
+    <|> do char '>'; return Greater
+    <|> do string "in"; requiredSpaces; return In
 
-factor :: Parser Exp
-factor = Const . IntVal <$> numConst
-       <|> Var <$> ident
-       <|> do symbol "("; f <- exprS; symbol ")"; return f
-       <|> do symbol "'"; f <- stringConst; symbol "'"; return $ Const (StringVal f)
+forClause :: Parser CClause
+forClause = do string "for"; requiredSpaces; i <- ident; string "in"; requiredSpaces; e <- expr; return (CCFor i e)
 
-relOp :: Exp -> Parser Exp
-relOp e = do symbol "=="; Oper Eq e      <$> exprS 
-      <|> do symbol ">";  Oper Greater e <$> exprS
-      <|> do symbol "<";  Oper Less e    <$> exprS
-      <|> do symbol "in"; Oper In e      <$> exprS
-      <|> return e
+ifClause :: Parser CClause
+ifClause = do string "if"; requiredSpaces; e <- expr; spaces; return (CCIf e)
 
-addOp :: Exp -> Parser Exp
-addOp e = do symbol "+"; Oper Plus e  <$> exprS
-      <|> do symbol "-"; Oper Minus e <$> exprS 
-      <|> return e
+requiredSpaces :: Parser ()
+requiredSpaces = do lookAhead (satisfy isSpace); spaces; return ()
 
-multOp :: Exp -> Parser Exp
-multOp e = do symbol "*";  Oper Times e <$> exprS 
-       <|> do symbol "//"; Oper Div e   <$> exprS
-       <|> do symbol "%";  Oper Mod e   <$> exprS
-       <|> return e
+clausez :: Parser [CClause]
+clausez = lexeme $ try (do lookAhead (char ']'); return [])
+    <|> do fc <- forClause; cs <- clausez; return (fc:cs)
+    <|> do ic <- ifClause; cs <- clausez; return (ic:cs)
 
-negOp :: Exp -> Parser Exp
-negOp e = do symbol "not"; negOp $ Not e
-      <|> return e
+exprz :: Parser [Exp]
+exprz = try (do lookAhead (oneOf ")]"); return [])
+    <|> do exprs
+
+exprs :: Parser [Exp]
+exprs = try (do e <- expr; symbol ","; es <- exprs; return (e:es))
+    <|> do e <- expr; return [e]
 
 ident :: Parser String
-ident = lexeme $ 
-        do 
-          i <- letter
-          is <- many (alphaNum <|> char '_')
-          if (i:is) `elem` reserved then fail "reserved word"
-            else return (i:is)
+ident = lexeme $ do lookAhead (noneOf "0123456789"); many1 isIdent;
+
+isIdent :: Parser Char
+isIdent = (isKeyword >> fail "reserved keyword")
+    <|> satisfy (\c -> isAlphaNum c || c == '_')
+
+isKeyword :: Parser ()
+isKeyword = try (do string "None"; spaces; oneOf "=("; return ())
+    <|> try (do string "True"; spaces; oneOf "=("; return ())
+    <|> try (do string "False"; spaces; oneOf "=("; return ())
+    <|> try (do string "for"; spaces; oneOf "=("; return ())
+    <|> try (do string "if"; spaces; oneOf "=("; return ())
+    <|> try (do string "in"; spaces; oneOf "=("; return ())
+    <|> try (do string "not"; spaces; oneOf "=("; return ())
 
 numConst :: Parser Int
-numConst = lexeme $ do n <- many1 digit; spaces; return $ read n
+numConst = lexeme $ do string "-"; ds <- getDigits; return (read ("-"++ds))
+    <|> do ds <- getDigits; return (read ds)
+
+getDigits :: Parser String
+getDigits = do char '0'; return "0"
+    <|> do lookAhead (noneOf "0"); many1 digit;
 
 stringConst :: Parser String
-stringConst = lexeme $ do s <- many1 letter; spaces; return $ read s
+stringConst = do s <- manyTill isStringConst (char '\''); return s
 
+isStringConst :: Parser Char
+isStringConst = newline
+    <|> (string "\\\\" >> return '\\') 
+    <|> (string "\\\'" >> return '\'') 
+    <|> (char '\\' >> fail "single backslash")
+    <|> do lookAhead $ satisfy isPrint; satisfy isAscii
 
--- do not use!!
--- whitespace :: Parser ()
--- whitespace = do _ <- space; _ <- newline; _ <- tab; return ()
+comment :: Parser ()
+comment = try (do char '#'; skipMany (noneOf "\\"); char '\n'; return ())
+    <|> do char '#'; skipMany (noneOf "\\"); eof
 
 lexeme :: Parser a -> Parser a
-lexeme p = do a <- p; spaces; do _ <- optional comments; return a
+lexeme p = try (do a <- p; comment; return a)
+    <|> do a <- p; spaces; return a
 
 symbol :: String -> Parser ()
-symbol ps = lexeme $ do _ <- try $ string ps; spaces
-
-
-reserved :: [String]
-reserved = ["None", "True", "False", "for", "if", "in", "not"]
-
-comments :: Parser String
-comments = do _ <- string "#"; _ <- manyTill anyChar newline; return ""
+symbol s = lexeme $ do string s; return () 
--- Assignment 3/code/part2/tests/Test.hs
+++ Assignment 3/code/part2/tests/Test.hs
@@ -9,12 +9,329 @@ import Test.Tasty.HUnit
 main :: IO ()
 main = defaultMain $ localOption (mkTimeout 1000000) tests
 
-tests = testGroup "Minimal tests" [
-  testCase "simple success" $
-    parseString "2 + two" @?=
-      Right [SExp (Oper Plus (Const (IntVal 2)) (Var "two"))],
-  testCase "simple failure" $
-    -- avoid "expecting" very specific parse-error messages
-    case parseString "wow!" of
-      Left e -> return ()  -- any message is OK
-      Right p -> assertFailure $ "Unexpected parse: " ++ show p]
+tests = testGroup "Parser tests" [
+  -- testCase "simple success" $
+  --   parseString "2 + two" @?=
+  --     Right [SExp (Oper Plus (Const (IntVal 2)) (Var "two"))],
+  -- testCase "simple failure" $
+  --   -- avoid "expecting" very specific parse-error messages
+  --   case parseString "wow!" of
+  --     Left e -> return ()  -- any message is OK
+  --     Right p -> assertFailure $ "Unexpected parse: " ++ show p]
+      
+      -- space tests
+      testCase "space at start" $
+        parseString " x" @?=
+          Right [SExp (Var "x")],
+      testCase "tab at start" $
+        parseString "\tx" @?=
+          Right [SExp (Var "x")],
+      testCase "newline at start" $
+        parseString "\nx" @?=
+          Right [SExp (Var "x")],
+
+      -- numConst tests
+      testCase "1" $
+        parseString "1" @?=
+          Right [SExp (Const (IntVal 1))],
+      testCase "-1" $
+        parseString "-1" @?=
+          Right [SExp (Const (IntVal (-1)))],
+      testCase "0 " $
+        parseString " 0 " @?=
+          Right [SExp (Const (IntVal (0)))],
+      testCase "-0" $
+        parseString "-0" @?=
+          Right [SExp (Const (IntVal (-0)))],
+      testCase "int overflow" $
+        parseString "18446744073709551615" @?=
+          Right [SExp (Const (IntVal (-1)))],
+      testCase "00" $
+        case parseString "00" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "007" $
+        case parseString "007" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "- 1" $
+        case parseString "- 1" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "+1" $
+        case parseString "+1" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+
+      -- ident tests
+      testCase "x" $
+        parseString "x" @?=
+          Right [SExp (Var "x")],
+      testCase "x=1" $
+        parseString "x=1" @?=
+          Right [SDef "x" (Const (IntVal 1))],
+      testCase "x = 1" $
+        parseString "x = 1" @?=
+          Right [SDef "x" (Const (IntVal 1))],
+      testCase "var" $
+        parseString "var" @?=
+          Right [SExp (Var "var")],
+      testCase "not1" $
+        parseString "not1" @?=
+          Right [SExp (Var "not1")],
+      testCase "false1" $
+        parseString "false1" @?=
+          Right [SExp (Var "false1")],
+      testCase "var_1" $
+        parseString "var_1" @?=
+          Right [SExp (Var "var_1")],
+      testCase "_var1" $
+        parseString "_var1" @?=
+          Right [SExp (Var "_var1")],
+      testCase "1_var" $
+        case parseString "1_var" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      --reserved words in ident
+      testCase "None=1" $
+        case parseString "None=1" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "True = 1" $
+        case parseString "True = 1" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase " False =   1" $
+        case parseString " False =   1" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "for" $
+        case parseString "for" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "if()" $
+        case parseString "if()" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "in=1" $
+        case parseString "in=1" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "not=1" $
+        case parseString "not=1" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+
+      -- stringConst tests
+      testCase "'a'" $
+        parseString "\'a\'" @?=
+          Right [SExp (Const (StringVal "a"))],
+      testCase "'hello world'" $
+        parseString "\'hello world\'" @?=
+          Right [SExp (Const (StringVal "hello world"))],
+      testCase "'!\"#$%&()*+,-./:;<=>?[]^_@`}{|~'" $ --TODO: ascii delete is printable?
+        parseString "'!\"#$%&()*+,-./:;<=>?[]^_@`}{|~'" @?=
+          Right [SExp (Const (StringVal"!\"#$%&()*+,-./:;<=>?[]^_@`}{|~"))],    
+      testCase "'x\\y\\z'" $
+        parseString "'x\\\\y\\\\z'" @?=
+          Right [SExp (Const (StringVal "x\\y\\z"))],
+      testCase "'x\\y\'z'" $
+        parseString "'x\\\\y\'z'" @?=
+          Right [SExp (Const (StringVal "x\\y'z"))],
+      testCase "'a\\nb'" $
+        parseString "'a\\nb'" @?=
+          Right [SExp (Const (StringVal "a\nb"))],
+      testCase "'fo\\\\o\\b\\na\\\'r'" $
+        parseString "'fo\\\\o\\b\\na\\\'r'" @?=
+          Right [SExp (Const (StringVal "fo\\ob\na'r"))],
+      testCase "'a\"b\\n'" $
+        parseString "'a\"b\\n'" @?=
+          Right [SExp (Const (StringVal "a\"b\\n"))],
+      testCase "'\\n'" $
+        parseString "'\n'" @?=
+          Right [SExp (Const (StringVal "\n"))],
+      testCase "'ë'" $
+        parseString "'ë'" @?=
+          Right [SExp (Const (StringVal "\235"))],
+      testCase "'\\t'" $
+        case parseString "'\t'" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "'\\DEL'" $
+        case parseString "'\DEL'" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "'\\'" $
+        case parseString "'\\'" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "'\''" $
+        case parseString "'\''" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+
+      -- other expr tests
+      testCase "NoneVal" $
+        parseString "None" @?=
+          Right [SExp (Const NoneVal)],
+      testCase "TrueVal" $
+        parseString "True " @?=
+          Right [SExp (Const TrueVal)],
+      testCase "FalseVal" $
+        parseString "False" @?=
+          Right [SExp (Const FalseVal)],
+          -- oper tested comprehensively below
+      testCase "not 1" $
+        parseString "not 1" @?=
+          Right [SExp (Not (Const (IntVal 1)))],
+      testCase "not not 1" $
+        parseString "not not 1" @?=
+          Right [SExp (Not (Not (Const (IntVal 1))))],
+      testCase "(1)" $
+        parseString "(1)" @?=
+          Right [SExp (Const (IntVal 1))],
+      testCase "(((1)))" $
+        parseString "(((1)))" @?=
+          Right [SExp (Const (IntVal 1))],
+      testCase "( 1 )" $
+        parseString "( 1 )" @?=
+          Right [SExp (Const (IntVal 1))],
+      testCase "f()" $
+        parseString "f()" @?=
+          Right [SExp (Call "f" [])],
+      testCase "f(x)" $
+        parseString "f(x)" @?=
+          Right [SExp (Call "f" [Var "x"])],
+      testCase "f ( x < 4 ) " $
+        parseString "f ( x < 4 ) " @?=
+          Right [SExp (Call "f" [Oper Less (Var "x") (Const (IntVal 4))])],
+      testCase "[]" $
+        parseString "[]" @?=
+          Right [SExp (List [])],
+      testCase "[1]" $
+        parseString "[1]" @?=
+          Right [SExp (List [Const (IntVal 1)])],
+      testCase "[1,2]" $
+        parseString "[1,2]" @?=
+          Right [SExp (List [Const (IntVal 1),Const (IntVal 2)])],
+      testCase "[1, 2, 3]" $
+        parseString "[1,2,3]" @?=
+          Right [SExp (List [Const (IntVal 1),Const (IntVal 2),Const (IntVal 3)])],
+      testCase "[(1+2), True, 'yes']" $
+        parseString "[(1+2), True, 'yes']" @?=
+          Right [SExp (List [Oper Plus (Const (IntVal 1)) (Const (IntVal 2)), Const (TrueVal), Const (StringVal "yes")])],
+      testCase "[x!=y,x>=y,x<=y,x not in y]" $
+        parseString "[x!=y,x>=y,x<=y,x not in y]" @?=
+          Right [SExp (List [Not (Oper Eq (Var "x") (Var "y")),Not (Oper Less (Var "x") (Var "y")),Not (Oper Greater (Var "x") (Var "y")),Not (Oper In (Var "x") (Var "y"))])],
+
+      -- oper tests
+      testCase "1+1" $
+        parseString "1+1" @?=
+          Right [SExp (Oper Plus (Const (IntVal 1)) (Const (IntVal 1)))],
+      testCase "1-1" $
+        parseString "1-1" @?=
+          Right [SExp (Oper Minus (Const (IntVal 1)) (Const (IntVal 1)))],
+      testCase "1 * 1" $
+        parseString "1 * 1" @?=
+          Right [SExp (Oper Times (Const (IntVal 1)) (Const (IntVal 1)))],
+      testCase "1 //1" $
+        parseString "1 //1" @?=
+          Right [SExp (Oper Div (Const (IntVal 1)) (Const (IntVal 1)))],
+      testCase "1% 1" $
+        parseString "1% 1" @?=
+          Right [SExp (Oper Mod (Const (IntVal 1)) (Const (IntVal 1)))],
+      testCase "1 == True" $
+        parseString "1 == True" @?=
+          Right [SExp (Oper Eq (Const (IntVal 1)) (Const TrueVal))],
+      testCase "1 != 1" $
+        parseString "1 != 1" @?=
+          Right [SExp (Not (Oper Eq (Const (IntVal 1)) (Const (IntVal 1))))],
+      testCase "1 < 1" $
+        parseString "1 < 1" @?=
+          Right [SExp (Oper Less (Const (IntVal 1)) (Const (IntVal 1)))],
+      testCase "1<=1" $
+        parseString "1<=1" @?=
+          Right [SExp (Not (Oper Greater (Const (IntVal 1)) (Const (IntVal 1))))],
+      testCase "1>1" $
+        parseString "1>1" @?=
+          Right [SExp (Oper Greater (Const (IntVal 1)) (Const (IntVal 1)))],
+      testCase "1>=1" $
+        parseString "1>=1" @?=
+          Right [SExp (Not (Oper Less (Const (IntVal 1)) (Const (IntVal 1))))],
+      testCase "1in [1]" $
+        parseString "1in [1]" @?=
+          Right [SExp (Oper In (Const (IntVal 1)) (List [Const (IntVal 1)]))],
+      testCase "1 not in [1]" $
+        parseString "1 not in [1]" @?=
+          Right [SExp (Not (Oper In (Const (IntVal 1)) (List [Const (IntVal 1)])))],
+      
+      -- clausez tests
+      testCase "[ 1for x in x ]" $
+        parseString "[ 1for x in x ]" @?=
+          Right [SExp (Compr (Const (IntVal 1)) [CCFor "x" (Var "x")])],
+      testCase "[ 1 for x in x if 1 == 1]" $
+        parseString "[ 1 for x in x if t == 1]" @?=
+          Right [SExp (Compr (Const (IntVal 1)) [CCFor "x" (Var "x"), CCIf (Oper Eq (Var "t") (Const (IntVal 1)))])],
+      testCase "[if x]" $
+        case parseString "[if x]" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+ 
+      -- keyword space tests
+      testCase "2 in[1]" $
+        case parseString "2 in[1]" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,      
+      testCase "[1 forx in x]" $
+        case parseString "[1 forx in x]" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "[1 for x inx]" $
+        case parseString "[1 for x inx]" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+      testCase "[1for x in x ifx]" $
+        case parseString "[1for x in x ifx]" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+
+      -- associativity tests
+      testCase "1-2+3" $
+        parseString "1-2+3" @?=
+          Right [SExp (Oper Plus (Oper Minus (Const (IntVal 1)) (Const (IntVal 2))) (Const (IntVal 3)))],
+      testCase "(1-2)+3" $
+        parseString "(1-2)+3" @?=
+          Right [SExp (Oper Plus (Oper Minus (Const (IntVal 1)) (Const (IntVal 2))) (Const (IntVal 3)))],
+      testCase "1-(2+3)" $
+        parseString "1-(2+3)" @?=
+          Right [SExp (Oper Minus (Const (IntVal 1)) (Oper Plus (Const (IntVal 2)) (Const (IntVal 3))))],
+      testCase "1*2//3" $
+        parseString "1*2//3" @?=
+          Right [SExp (Oper Div (Oper Times (Const (IntVal 1)) (Const (IntVal 2))) (Const (IntVal 3)))],
+      testCase "(1*2)//3" $
+        parseString "(1*2)//3" @?=
+          Right [SExp (Oper Div (Oper Times (Const (IntVal 1)) (Const (IntVal 2))) (Const (IntVal 3)))],
+      testCase "1*(2//3)" $
+        parseString "1*(2//3)" @?=
+          Right [SExp (Oper Times (Const (IntVal 1)) (Oper Div (Const (IntVal 2)) (Const (IntVal 3))))],
+      testCase "x<y<z" $
+        case parseString "x<y<z" of
+          Left e -> return ()
+          Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+
+      -- statement tests
+      testCase "x; x=1;1" $
+        parseString "x; x=1;1" @?=
+          Right [SExp (Var "x"), SDef "x" (Const (IntVal 1)), SExp (Const (IntVal 1))],
+
+      -- comment tests
+      testCase "x#bar" $
+        parseString "x#bar" @?=
+          Right [SExp (Var "x")],
+      testCase "x#\\n=1" $
+        parseString "x#\\nbar" @?=
+          Right [SDef "x" (Const (IntVal 1))],
+      testCase "x#bar 78 \\n = 1" $
+        parseString "x#bar 78 \\n = 1" @?=
+          Right [SDef "x" (Const (IntVal 1))]
+    ]
