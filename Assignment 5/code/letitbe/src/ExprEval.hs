6846038e0c2c988853ccbc966b25aba0df8b28f1
albeate <blaadino@protonmail.com>
2021-10-11 17:20:22 +0200

    expr

--- Assignment 5/code/letitbe/src/ExprEval.hs
+++ Assignment 5/code/letitbe/src/ExprEval.hs
@@ -13,7 +13,7 @@ oper Times = (*)
 
 eval :: Expr -> Env -> Either String Int
 eval (Const n) env = return n
-eval (Oper op x y) env = (oper op) <$> eval x env <*> eval y env
+eval (Oper op x y) env = oper op <$> eval x env <*> eval y env
 eval (Var v) env = case M.lookup v env of
                      Nothing -> Left ("Unknown identifier: "++v)
                      Just val -> return val
@@ -21,14 +21,30 @@ eval (Let v e body) env = do
   val <- eval e env
   eval body $ M.insert v val env
 
+evalTop :: Expr -> Either String Int
 evalTop e = eval e M.empty
 
+simplify :: Expr -> Expr
 simplify e =
   case e of
     Oper Plus (Const c1) (Const c2) -> Const(c1+c2)
-    Oper Minus (Const c1) (Const c2) -> Const(c1+c2)
+    Oper Minus (Const c1) (Const c2) -> Const(c1-c2) -- changed from (+) to (-)
     Oper Times (Const c1) (Const c2) -> Const(c1*c2)
     Oper op e1 e2 -> Oper op (simplify e1) (simplify e2)
     Let v e body ->
       Let v (simplify e) (simplify body)
     _ -> e
+
+{-
+simplify (Oper Minus (Oper Plus (Const 5) (Const 6)) (Const 2))
+ -> results in: Oper Minus (Const 11) (Const 2)
+
+evalTop (Oper Minus (Oper Plus (Const 5) (Const 6)) (Const 2))
+ -> results in: Right 9
+
+simplify (Let "x" (Const 3) (Oper Times (Var "x") (Var "x")))
+  -> results in: Let "x" (Const 3) (Oper Times (Var "x") (Var "x"))
+
+evalTop (Let "x" (Const 3) (Oper Times (Var "x") (Var "x")))
+  -> results in: Right 9
+-}
